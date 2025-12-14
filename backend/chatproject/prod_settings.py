19aa669672a1326d78491f581a4c52ddeabc842d
ahamedajaj01 <ahamed.ajaj01@gmail.com>
2025-12-14 23:55:26 +0545

    new settings added for prod

--- backend/chatproject/prod_settings.py
+++ backend/chatproject/prod_settings.py
@@ -53,15 +53,11 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [
-                {
-                    "address": os.getenv("REDIS_URL"),
-                    "ssl": True,
-                }
-            ],
+            "hosts": [os.getenv("REDIS_URL")],
         },
     },
 }
+
 
 
 MIDDLEWARE = [
