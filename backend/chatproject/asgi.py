04406f7034530cf53f515ea5d809beeaa9216fb2
ahamedajaj01 <ahamed.ajaj01@gmail.com>
2025-12-15 00:07:45 +0545

    upated in asgi

--- backend/chatproject/asgi.py
+++ backend/chatproject/asgi.py
@@ -12,8 +12,6 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatproject.settings')
-
 django_asgi_app = get_asgi_application()
 
 from chatapp.routing import websocket_urlpatterns
