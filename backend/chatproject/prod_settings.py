47e2a5303e2286499bff2ebd59d9477ece18e422
ahamedajaj01 <ahamed.ajaj01@gmail.com>
2025-12-30 13:56:57 +0545

    fixed email sending problem

--- backend/chatproject/prod_settings.py
+++ backend/chatproject/prod_settings.py
@@ -237,3 +237,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+if not DEFAULT_FROM_EMAIL:
+    raise ValueError("DEFAULT_FROM_EMAIL is not set in environment variables")
--- backend/chatproject/settings.py
+++ backend/chatproject/settings.py
@@ -207,4 +207,4 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 # EMAIL_USE_TLS = True
 # EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 # EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-# DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
