12a2de5386ee3f0d5a93dfe70735457f05138096
ahamedajaj01 <ahamed.ajaj01@gmail.com>
2026-01-20 19:58:19 +0545

    fixes config setting

--- backend/chatproject/prod_settings.py
+++ backend/chatproject/prod_settings.py
@@ -117,7 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
-AUTH_USER_MODEL = "accounts.User"
+AUTH_USER_MODEL = "chatapp.User"
 
 
 REST_FRAMEWORK = {
